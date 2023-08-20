877a9235d270d66c126ab08e0e6adf4738691624
Phidias <124069538+Phidias2@users.noreply.github.com>
2023-08-20 01:02:54 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import Coupon from './Pages/Coupon.jsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    // element: <Accueil />
+    element: <Login />
   },
   {
     path: "/login",
